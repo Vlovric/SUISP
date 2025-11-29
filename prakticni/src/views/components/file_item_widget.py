342944d25c4f499312abcd4d7fde3ac815fb4fa8
dmatijani <dmatijani21@foi.hr>
2025-11-29 13:59:40 +0100

    Malo sređen UI

--- prakticni/src/views/components/file_item_widget.py
+++ prakticni/src/views/components/file_item_widget.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout
 from PySide6.QtGui import QIcon
+from datetime import datetime
 
 class FileItemWidget(QWidget):
     def __init__(self, file_record):
@@ -12,9 +13,9 @@ class FileItemWidget(QWidget):
         date_text = ""
         date_modified = file_record["date_modified"]
         if date_modified:
-            date_text = f"Ažurirana: {date_modified}"
+            date_text = f"Ažurirana: {self.format_date(date_modified)}"
         else:
-            date_text = f"Prenesena: {file_record['date_uploaded']}"
+            date_text = f"Prenesena: {self.format_date(file_record['date_uploaded'])}"
 
         self.date_label = QLabel(date_text)
 
@@ -22,19 +23,30 @@ class FileItemWidget(QWidget):
         left.addWidget(self.name_label)
         left.addWidget(self.date_label)
 
+        button_style = "QPushButton { padding: 0px; margin: 0px; }"
+
         self.btn_export = QPushButton()
         self.btn_export.setIcon(QIcon("src/pic/download.svg"))
         self.btn_export.setToolTip("Izvezi datoteku")
+        self.btn_export.setFixedSize(40, 40)
+        self.btn_export.setStyleSheet(button_style)
+        self.btn_export.setProperty("class", "success")
         self.btn_export.clicked.connect(self.handle_export)
         
         self.btn_delete = QPushButton()
         self.btn_delete.setIcon(QIcon("src/pic/delete.svg"))
         self.btn_delete.setToolTip("Sigurnosno obriši datoteku")
+        self.btn_delete.setFixedSize(40, 40)
+        self.btn_delete.setStyleSheet(button_style)
+        self.btn_delete.setProperty("class", "danger")
         self.btn_delete.clicked.connect(self.handle_delete)
 
         self.btn_share = QPushButton()
         self.btn_share.setIcon(QIcon("src/pic/share.svg"))
         self.btn_share.setToolTip("Dijeli datoteku")
+        self.btn_share.setFixedSize(40, 40)
+        self.btn_share.setStyleSheet(button_style)
+        self.btn_share.setProperty("class", "info")
         self.btn_share.clicked.connect(self.handle_share)
 
         right = QHBoxLayout()
@@ -51,6 +63,9 @@ class FileItemWidget(QWidget):
         layout.addLayout(right)
 
         self.setLayout(layout)
+
+    def format_date(self, date_text):
+        return datetime.fromisoformat(date_text).strftime('%d.%m.%Y. %H:%M:%S')
 
     def handle_export(self):
         # TODO implementirati
--- prakticni/src/views/pregled_datoteka/pregled_datoteka_view.py
+++ prakticni/src/views/pregled_datoteka/pregled_datoteka_view.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QLabel, QScrollArea, QFrame, QVBoxLayout, QPushButton
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QIcon
 
 from src.views.components.file_item_widget import FileItemWidget
 
@@ -22,12 +23,18 @@ class PregledDatotekaView(QWidget):
 
         self.scroll_area.setWidget(self.list_container)
 
-        self.import_btn = QPushButton("Unesi")
+        self.import_btn = QPushButton("Prenesi")
+        self.import_btn.setIcon(QIcon("src/pic/upload.svg"))
+        self.import_btn.setToolTip("Prenesi datoteku")
+
+        bottom_layout = QVBoxLayout()
+        bottom_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        bottom_layout.addWidget(self.import_btn, alignment=Qt.AlignHCenter)
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.title)
         main_layout.addWidget(self.scroll_area)
-        main_layout.addWidget(self.import_btn)
+        main_layout.addLayout(bottom_layout)
 
         self.setLayout(main_layout)
 
