51b8a4b69c4a612a2b8090351434faa4ff84241d
Viktor Lovric <vlovric21@student.foi.hr>
2025-11-29 22:44:57 +0100

    Napravljeno stavljanje u focus

--- prakticni/src/utils/single_application.py
+++ prakticni/src/utils/single_application.py
@@ -1,13 +1,15 @@
 from PySide6.QtWidgets import QApplication
 from PySide6.QtNetwork import QLocalServer, QLocalSocket
+from PySide6.QtCore import Signal
 
 
 class SingleApplication(QApplication):
+    activateRequested = Signal()
+
     def __init__(self, argv, app_id):
         super().__init__(argv)
         self.app_id = app_id
         self.socket = QLocalSocket()
-
         self.socket.connectToServer(self.app_id)
 
         if self.socket.waitForConnected(500):
@@ -24,24 +26,21 @@ class SingleApplication(QApplication):
             self.is_running = True
             return
         
-        else:
-            self.is_running = False
-            self.socket.close()
-            self._server = QLocalServer(self)
+        self.is_running = False
+        self.socket.close()
+        self._server = QLocalServer(self)
 
-            self._server.removeServer(self.app_id)
-            if not self._server.listen(self.app_id):
-                raise RuntimeError(f"Unable to start the server: {self._server.errorString()}")
+        self._server.removeServer(self.app_id)
+        if not self._server.listen(self.app_id):
+            raise RuntimeError(f"Unable to start the server: {self._server.errorString()}")
 
-            self._server.newConnection.connect(self._handle_new_connection)
+        self._server.newConnection.connect(self._handle_new_connection)
 
     def _handle_new_connection(self):
-        new_socket = self._server.nextPendingConnection()
+        socket = self._server.nextPendingConnection()
         try:
-            window = self.activeWindow()
-            if window:
-                    window.show()
-                    window.raise_()
-                    window.activateWindow()
+            _ = socket.readAll()
+            self.activateRequested.emit()
+
         finally:
-            new_socket.close()
+            socket.close()
