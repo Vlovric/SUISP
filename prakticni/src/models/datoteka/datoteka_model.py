e1ae491ea99230e8e9857a95420e868151e2b6e5
dmatijani <dmatijani21@foi.hr>
2025-12-03 11:34:20 +0100

    Može se izvesti package za dijeljenje datoteke

--- prakticni/src/models/datoteka/datoteka_model.py
+++ prakticni/src/models/datoteka/datoteka_model.py
@@ -7,6 +7,11 @@ class DatotekaModel:
         return db.fetch_all(query)
     
     @staticmethod
+    def get_file(id):
+        query = "SELECT * FROM file WHERE ID = ? LIMIT 1"
+        return db.fetch_one(query, (id,))
+    
+    @staticmethod
     def insert_file_entry(name, path, is_binary, date_uploaded, dek_encrypted, hash):
         query = "INSERT INTO file (name, path, binary, date_uploaded, dek_encrypted, hash) VALUES (?, ?, ?, ?, ?, ?)"
         return db.execute_query(query, (name, path, is_binary, date_uploaded, dek_encrypted, hash))
